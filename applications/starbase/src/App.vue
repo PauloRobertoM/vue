ccd0966011ba9afd3ac06d93d517ab6f89129e42
Paulo Alves <paulo1rm23@outlook.com>
2018-01-10 13:01:39 -0300

    Aula 27

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<h3>{{title}}</h3>
+		<button class="btn btn-primary" @click="fetchCharacter">Busca</button>
 	</div>
 </template>
 
@@ -10,6 +11,15 @@
 		data() {
 			return {
 				title: 'Gere o seu time'
+			}
+		},
+		methods: {
+			fetchCharacter() {
+				fetch('https://swapi.co/api/people/1', {
+					method: 'GET'
+				})
+					.then(response => response.json())
+					.then(json => console.log(json))
 			}
 		}
 	}
