34072f9fd3db638bb4aefbdfe018052aa2b6a337
Paulo Alves <paulo1rm23@outlook.com>
2018-01-10 15:30:32 -0300

    Aula 39

---
+++ applications/starbase-router/src/components/Item.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="col-md-4">
+		<h4 class="card-title">{{item.name}}</h4>
+		<div v-for="(value, key, index) in item">
+			<div v-if="index < 5">
+				<strong>{{key}}</strong>: {{value}}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		props: ['item']
+	}
+</script>
